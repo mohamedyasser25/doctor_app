32a238efee1b96f31e3ce08683a2ace95563a351
mohamedyassernabet <mohamedyassernabet@gmail.com>
2024-07-28 21:37:03 +0300

    * add images * add flutter_util * add a file for doc_app client * add navigation extention * add login screen  * add colors & stayels  * add onboarding Screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/features/login/ui/login_screen.dart';
+import 'package:doctor_app/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text(' no Route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/features/onboarding/onboarding_screen.dart
@@ -0,0 +1,48 @@
+import 'package:doctor_app/core/theming/styles.dart';
+import 'package:doctor_app/features/onboarding/widgets/doc_logo_and_name.dart';
+import 'package:doctor_app/features/onboarding/widgets/doctor_text_and_image.dart';
+import 'package:doctor_app/features/onboarding/widgets/get_started_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+            child: Column(
+              children: [
+                const DocLogoAndName(),
+                SizedBox(
+                  height: 30.h,
+                ),
+                const DoctorTextAndImage(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                        style: TextStyles.font13GrayRegular,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      const GetStartedButton(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
